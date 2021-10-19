195c6484bb77bbe8c89a9386eb684464b965a7fe
cqr <1145536446@qq.com>
2021-10-19 22:05:48 +0800

    feat:完成基本交互功能

---
+++ js/handleFile.js
@@ -0,0 +1,80 @@
+let fs = require('fs')
+/**
+ * 异步写入文件
+ * arr: 读取数据的数组
+ * type: 写入文件类型（0-->写入题目，1-->写入答案, 2-->写入成绩）
+ * callback: 回调函数
+ * @param Object, number, function
+ */
+
+function writeFile(arr,type) {
+  return new Promise((resolve,reject) => {
+    if(!arr) {
+      return reject('null file')
+    }
+    let len = arr.length
+    let str = ''
+    let path = ''
+    for(let i = 0 ; i < len ; i++){
+      if(i == len-1){
+        str += arr[i]
+      }else{
+        str += arr[i] + ',\r\n'
+      }
+    }
+    if(type === 0){
+      path = './static/exercisefile.txt'
+    }
+    else if(type === 1){
+      path = "./static/answerfile.txt"
+    }
+    else if(type === 2){
+      path = "./static/Grade.txt"
+    }
+    else{
+      return reject('parameter error')
+    }
+    fs.writeFile(path,str,err => {
+      if(err){
+        reject(err)
+      }else{
+        resolve()
+      }
+    })
+  })
+}
+/**
+ * 同步读取文件，返回文件字符串数组
+ * type: 读取文件类型（0-->读取题目，1-->读取答案，2-->读取成绩）
+ * @param Object, number
+ */
+function readFile(type) {
+  if (type == 0) {
+		path = "./static/exercisefile.txt";
+	} else if (type == 1) {
+		path = "./static/answerfile.txt";
+	} else {
+		return console.error("parameter error");
+	}
+	return fs.readFileSync(path).toString().split(',\r\n');
+}
+/**
+ * 获取填写的答案数组
+ * 参数为题目文件读出来的数组，即 getWriteArr(readFile(0))
+ * @param Array
+ */
+function getWriteArr(exerciseArr) {
+	let len = exerciseArr.length;
+	let arr = [];
+	for(let i = 0; i < len; i++) {
+		arr.push(exerciseArr[i].split('=')[1].replace(/\s+/g,""));
+	}
+	return arr;
+	// 返回格式： [ '1' , '2', '3']，对应题目顺序
+}
+
+module.exports = {
+    writeFile: writeFile,
+    readFile: readFile,
+    getWriteArr: getWriteArr
+}
--- js/start.js
+++ js/start.js
@@ -1,5 +1,148 @@
 // 主要函数
-const {a} = require('./js/test.js');
+let {writeFile, readFile, getWriteArr} = require("./js/handleFile.js")
+//主要数据
+let topicArr = ['444444sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss4','55555555555','888888'] //存放题目的数组
+let answerArr = []   //存放答案的数组
+let gradeArr = []    //存放结果的数组
+let topicNum = 10 //题目数量，初始化为10
+let isTopicShow = false //是否有题目出现
 
-// 测试一下
-console.log(a);
+
+//获得主界面的元素
+let con = document.getElementsByClassName('body-context')[0]
+
+//监听页面的点击事件
+con.onclick = (e) => {
+
+  //获取点击的元素
+  let target = e.target
+  let clickFuncName = target.getAttribute('click')
+
+  //根据函数名来判断要执行哪些函数
+  if(clickFuncName === 'createTopic'){
+    changeTopicNum()
+  }  
+  else if(clickFuncName === 'commit'){
+    commit()
+    // console.log('commit')
+  }
+}
+
+//1、 切换题目数量
+
+//获取展示题目的元素
+let topicCon = document.getElementsByClassName('topic-context')[0]
+
+const changeTopicNum = () => {
+  //获取题目数量
+  let inputNumVal = document.getElementsByClassName('topic-num-input')[0].value
+  let attention = document.getElementsByClassName('attention')[0]
+  //去除原来的题目
+  if(topicCon.innerHTML !== ''){
+    topicCon.innerHTML = ''
+  }
+  let judgeRes =  judgeInputVal(inputNumVal,attention)
+  if(judgeRes){
+      //更新全局变量的题目数量
+      topicNum = inputNumVal
+      createTopic(inputNumVal)
+      isTopicShow = true
+  }
+}
+
+//判断用户的题目数量输入是否合法
+const judgeInputVal = (inputVal,target) => {
+  let reg = /(^[1-9]\d*$)/
+  if(inputVal === ''){
+    poinError('请输入题目数量',target)
+    isTopicShow = false
+    return false
+  }
+  else if(!reg.test(inputVal)){
+    poinError('请输入正整数',target)
+    isTopicShow = false
+    return false
+  }
+  else if(inputVal > 10000){
+    poinError('输入值不能大于10000',target)
+    isTopicShow = false
+    return false
+  }
+  else{
+    target.style.display = 'none'
+    return true
+  }
+}
+
+//提示用户输入错误
+const poinError = (msg,target) => {
+  
+   let htmlStr = 
+    `
+    <h3>${msg}</h3>
+    `
+    target.innerHTML = htmlStr
+    target.style.display = 'block'
+}
+
+//生成题目与答案
+const createTopic = (num) => {
+  showTopic(topicArr)
+  writeFile(topicArr,0).then(() => {
+    console.log(readFile(0))
+  })
+}
+
+//显示题目
+const showTopic = (topics) => {
+  let htmlStr = ''
+  let len = topics.length
+  topicCon.innerHTML = ''
+  for(let i = 0 ; i < len ; i++){
+    htmlStr += 
+		`<span class="topic">
+			<span>${i+1}、  </span>	
+			<p>${topics[i]}</p>
+			<input class="topic-write" type="text">
+			<div class="topic-judeg"></div>
+		</span>`
+  }
+  topicCon.innerHTML = htmlStr
+}
+
+//提交答案
+const commit = () => {
+  if(!isTopicShow){
+    return 
+  }
+  let writeArr = []
+  let writeDomArr = document.getElementsByClassName('topic-write')
+  let len =  writeDomArr.length
+  for(let i = 0; i < len; i++) {
+		writeArr.push(writeDomArr[i].value);
+	}
+
+  //判断答案是否正确
+  judeg(writeArr, answerArr)
+}
+
+//验证函数
+const judeg = (writeArr, answerArr) => {
+  console.log('write:', writeArr)
+	console.log('answer:', answerArr)
+  let len = writeArr.length
+  let topicJudgeArr = document.getElementsByClassName('topic-judeg')
+  let correctArr = []
+	let wrongArr = []
+  for(let i = 0 ; i < len ; i++){
+    if(true){
+      topicJudgeArr[i].innerHTML = '√'
+      topicJudgeArr[i].style.color = '#080'
+      correctArr.push(i+1)
+    }else{
+      topicJudgeArr[i].innerHTML = 'X'
+      topicJudgeArr[i].style.color = '#f40'
+      wrongArr.push(i+1)
+    }
+  }
+}
